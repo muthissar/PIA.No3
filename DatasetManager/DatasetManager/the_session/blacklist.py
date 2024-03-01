cc73e4585856ea69da9b7382d8e26a6b3f9d193b
Mathias Rose Bjare <muthissar@gmail.com>
2024-03-01 16:59:38 +0100

    Removed again the repos, which should not be included here...

--- DatasetManager/DatasetManager/the_session/blacklist.py
+++
@@ -1,5 +0,0 @@
-tune_10828.abc -> "slide"
-
-TODO add5 chords
-abcFormat: WARNING: Could not get pitch information from note:  Â, assuming C
-abcFormat: WARNING: Could not get pitch information from note:  Â, assuming C
--- usertest/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE result (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    piece TEXT,
-    correct_exp TEXT,
-    correct_sample INTEGER,
-    selected_exp TEXT,
-    selected_sample INTEGER
-);
